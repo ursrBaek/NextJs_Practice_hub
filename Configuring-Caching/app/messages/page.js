63d7ca135b8e0f9174efb1f02c2b6e85d80f52ef
ursrBaek <ursrbaek@gmail.com>
2024-07-25 13:42:56 +0900

    캐시 무효화 설정

--- Configuring-Caching/app/messages/page.js
+++ Configuring-Caching/app/messages/page.js
@@ -1,9 +1,13 @@
-import Messages from '@/components/messages';
+import { unstable_noStore } from "next/cache";
+import Messages from "@/components/messages";
+
+// export const revalidate = 5;
+// export const dynamic = "force-dynamic";
 
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
-    headers: {
-      'X-ID': 'page',
+  const response = await fetch("http://localhost:8080/messages", {
+    next: {
+      tags: ["msg"],
     },
   });
   const messages = await response.json();
