b7abac95e7f015a2805709ff8f9c5219443b5bf8
ursrBaek <ursrbaek@gmail.com>
2024-07-26 13:53:25 +0900

    이미지 설정, 이미지 로더 및 클라우드 크기 조정

--- App-Optimizations-prac/components/header.js
+++ App-Optimizations-prac/components/header.js
@@ -1,11 +1,19 @@
 import logo from "@/assets/logo.png";
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Header() {
   return (
     <header id="main-header">
       <Link href="/">
-        <img src={logo.src} alt="Mobile phone with posts feed on it" />
+        <Image
+          src={logo}
+          width={100}
+          height={100}
+          // sizes="10vw"
+          priority
+          alt="Mobile phone with posts feed on it"
+        />
       </Link>
       <nav>
         <ul>
